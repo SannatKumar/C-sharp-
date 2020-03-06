e2f7a32f4e3b2c147558a07a88b4ed2699743ea9
Raj Kumar <interplaytheheads@gmail.com>
2020-03-06 15:27:35 +0200

    Description display and User Input ready

--- Guess the bird/Program.cs
+++ Guess the bird/Program.cs
@@ -10,10 +10,18 @@ namespace Guessthebird
     {
         static void Main(string[] args)
         {
-            WelcomeUSer();
+            //Function call to greet the user
+            WelcomeUser();
+
+            //Function to print the messages in different colours
+            PrintColorMessage(ConsoleColor.Red, "Hei you");
+
+            //Get the details of the app
             GetAppDetails();
                                  
         }
+
+        //function to get the app detail
         static void GetAppDetails()
         {
             //Initialize the app info
@@ -26,7 +34,9 @@ namespace Guessthebird
             Console.WriteLine("App Version: {0}", appVersion);
             Console.WriteLine("App Author: {0}", appAuthor);
         }
-        static void WelcomeUSer()
+        
+        //function to greet the user
+        static void WelcomeUser()
         {
             //Take the user name
             Console.WriteLine("What is your name?");
@@ -36,6 +46,19 @@ namespace Guessthebird
 
             //Printing the Player name and game description
             Console.WriteLine("Hei {0}, Lets play a fun game/n where you have to choose a bird name and match the guess with the machine.", PlayerName);
+        }
+
+        //function to change the color and display messages
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            //change the color
+            Console.ForegroundColor = color;
+
+            //print the message
+            Console.WriteLine(message);
+
+            //Reset the color
+            Console.ResetColor();
         }
     }
 }
