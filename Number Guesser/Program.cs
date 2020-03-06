bb1b0f2ff7d4809d21c41d9b47d5f16d1aee1296
Raj Kumar <interplaytheheads@gmail.com>
2020-03-06 12:02:30 +0200

    Initial Commit

---
+++ Number Guesser/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Namespace
+namespace NumberGuesser
+{
+     // Main class
+    class Program
+    {
+         //Entry Point method
+        static void Main(string[] args)
+        {
+            string name = "Raj KUmar Tiwari";
+            int age = 31;
+            // START HERE //
+            Console.WriteLine("Hello World");
+            Console.Write("HEi YOu");
+            Console.WriteLine("{0} is {1}", name, age);
+        }
+    }
+}
