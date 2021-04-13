978c0f7e7d014d47e0c984f16221b2355a13bd3d
Raj Kumar <interplaytheheads@gmail.com>
2021-04-13 21:16:02 +0300

    deposit and withdrawl function added

--- material/classes/BankAccount.cs
+++ material/classes/BankAccount.cs
@@ -6,38 +6,73 @@ using System.Threading.Tasks;
 
 namespace classes
 {
-        public class BankAccount
+    public class BankAccount
+    {
+
+        //member declaration for the bank account number
+
+        private static int accountNumberSeed = 1234567890;
+
+        //properties of the class BankAccount
+        public string Number { get; set; }
+        public string Owner { get; set; }
+        public decimal Balance
         {
-
-            //member declaration for the bank account number
-            
-            private static int accountNumberSeed = 1234567890;
-
-            //properties of the class BankAccount
-            public string Number { get; set; }
-            public string Owner { get; set; }
-            public decimal Balance { get; }
-
-            //methods of the class BankAccount
-            public void MakeDeposit(decimal amount, DateTime date, string note)
+            get
             {
-
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
             }
+        }
 
-            //Constructor assigning the values
 
-            public BankAccount(string name, decimal initialBalance)
-            {
-                this.Owner = name;
-                this.Balance = initialBalance;
-                this.Number = accountNumberSeed.ToString();
-                accountNumberSeed++;
-            }
+        //Constructor assigning the values
 
-            public void MakeWithdrawl(decimal amount, DateTime date, string note)
-            {
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
 
-            }
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance.");
 
         }
+
+        // List of Objects
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        //methods of the class BankAccount
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+
+        //Methods to withdraw
+        public void MakeWithdrawl(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Ampunt of withdrawl must be Positive")
+            }
+            if(Balance - amount<0)
+            {
+                throw new InvalidOperationException("Not Sufficient funds for this withdrawl.");
+            }
+            var withdrawl = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawl);
+        }
+
+    }
 }
