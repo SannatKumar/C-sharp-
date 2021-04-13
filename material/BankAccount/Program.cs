2ae4c90f3247714cf7231b3e72a751b85770137d
Raj Kumar <interplaytheheads@gmail.com>
2021-04-13 16:35:12 +0300

    Object instantiated with class

--- material/BankAccount/Program.cs
+++ material/BankAccount/Program.cs
@@ -6,15 +6,38 @@ using System.Threading.Tasks;
 
 namespace BankAccount
 {
+    
+    public class Person
+    {
+        //Fields
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        //Methods
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My Name is " + Name + " and My age is " + Age);
+            Console.ReadKey();
+        }
+    }
+    
+    //Main Class Program
     class Program
     {
         static void Main(string[] args)
         {
-            public class BankAccount
-            {
-                
+            Person person = new Person();
 
-            }
+            person.Name = "Sunil";
+            person.Age = 26;
+            person.HasPet = true;
+
+
+            person.Greeting();
+
+
+
         }
     }
 }
