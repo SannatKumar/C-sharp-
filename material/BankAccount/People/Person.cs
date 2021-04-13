d675f5e02bbd14010152efad14e95cdc9f4bba5d
Raj Kumar <interplaytheheads@gmail.com>
2021-04-13 16:45:15 +0300

    Folder and files for grouping

---
+++ material/BankAccount/People/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.People
+{
+    public class Person
+    {
+        //Fields
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        //Methods
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My Name is " + Name + " and My age is " + Age);
+            Console.ReadKey();
+        }
+    }
+}
