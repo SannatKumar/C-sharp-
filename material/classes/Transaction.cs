3f1a0155134d464d2a0cce46ab64f95e9e82c8e9
Raj Kumar <interplaytheheads@gmail.com>
2021-04-13 20:52:42 +0300

    values defined with constructor

--- material/classes/Transaction.cs
+++ material/classes/Transaction.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace classes
 {
-    class Transaction
+    public class Transaction
     {
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
     }
 }
