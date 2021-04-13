3621097e6d227184fdd51ebea5250e1615d7b331
Raj Kumar <interplaytheheads@gmail.com>
2021-04-13 21:23:27 +0300

    Readkey added

--- material/classes/Program.cs
+++ material/classes/Program.cs
@@ -13,6 +13,13 @@ namespace classes
             var account = new BankAccount("Raj Kumar", 1000);
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance}");
             Console.ReadKey();
+
+            account.MakeWithdrawl(500, DateTime.Now, "Rent Payment");
+            Console.WriteLine(account.Balance);
+            Console.ReadKey();
+            account.MakeDeposit(100, DateTime.Now, "Friend Paid me Back");
+            Console.WriteLine(account.Balance);
+            Console.ReadKey();
         }
     }
 }
