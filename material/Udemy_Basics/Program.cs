c46f0f29caf328a6352371e204f2465be696c8dc
Raj Kumar <interplaytheheads@gmail.com>
2021-04-15 00:36:05 +0300

    Explicit and Implicit typecasting

---
+++ material/Udemy_Basics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hei Bud");
+            // Ask user for fave number
+            Console.Write("Enter your favorite number!: ");
+
+
+
+            // Turn that answer into an int
+
+            //Attempt 1: use implicit conversion
+            //int faveNumber = Console.ReadLine();
+
+            //Attempt 2: use explicit conversion
+            int faveNumber = (int)Console.ReadLine();
+
+            //Attempt 3: use Convert method
+
+            int faveNumber = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
