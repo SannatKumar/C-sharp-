c20f6654394d505a90f77079bdf46cfa06ec6027
Raj Kumar <interplaytheheads@gmail.com>
2021-04-27 23:47:23 +0300

    Incrementing and Decrementing

---
+++ material/Codeacademy_exercises/MakingProgress.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakingProgress
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // declare steps variable
+      int steps = 0;
+
+      // Two steps forward
+
+      steps = steps + 2;
+
+
+      // One step back 
+      steps = steps - 1;
+
+
+      // Print result to the console
+
+      Console.WriteLine(steps);
+
+    }
+  }
+}
