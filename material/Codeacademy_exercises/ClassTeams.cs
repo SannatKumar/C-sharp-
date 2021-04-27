49808cf537a21c460d8f438c8ebd2e18c7eac603
Raj Kumar <interplaytheheads@gmail.com>
2021-04-27 23:52:38 +0300

    Use of modulo to group the class

---
+++ material/Codeacademy_exercises/ClassTeams.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTeams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Number of students
+
+            int students = 18;
+
+
+            // Number of students in a group
+            int groupSize = 3;
+
+
+            // Does groupSize go evenly into students?
+
+            Console.WriteLine(students % groupSize);
+
+
+        }
+    }
+}
