e6215846e154ebe5c39977939b209d8d74dfe241
Raj Kumar <interplaytheheads@gmail.com>
2021-04-28 21:17:18 +0300

    Use of Math functions

---
+++ material/Codeacademy_exercises/LowestNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowestNumber
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Starting variables 
+      int numberOne = 12932;
+      int numberTwo = -2828472;
+
+      // Use built-in methods and save to variable 
+      double numberOneSqrt = Math.Floor(Math.Sqrt(numberOne));
+      double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+
+      // Use built-in methods and save to variable
+
+
+
+      // Print the lowest number
+      Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+
+
+    }
+  }
+}
